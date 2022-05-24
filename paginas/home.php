1518b10e9b985ad59b896ef217f62cf0a196e576
LUIZ FELIPE RADOLL CORDEIRO <luiz.cordeiro2@alunos.sc.senac.br>
2022-05-24 11:36:59 -0300

    header completo

--- paginas/home.php
+++ paginas/home.php
@@ -3,7 +3,11 @@
 ?>
 <body>
     <header>
-        <img src="./../img/logo.png" class="logo">
-        <p><a href=""><img>informe o CEP <img src="./../img/pointer.png"></a></p>
+        <a href="home.php"><img src="../img/logo.png" class="logo"></a>
+        <h2><a href="" class="local-href"><img src="../img/pointer.png" class="pointer"> informe seu CEP</a></h2>
+        <input type="text" class="pesq" placeholder="Pesquisar livro...">
+        <h2 class="logh2"><a href="login.php" class="log">Login</a></h2>
+        <a href=""><img src="../img/carrinho.png" class="carrinho"></a>
+        <div class="clear"></div>
     </header>
 </body>
